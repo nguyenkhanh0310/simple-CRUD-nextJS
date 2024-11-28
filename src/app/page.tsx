b66409cb8573f9c8802777570e99b05773ce629e
nguyenkhanh0310 <mykhanh03102003@gmail.com>
2024-11-28 15:43:15 +0700

    useSWR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,38 @@ import Link from "next/link";
 import x from "@/styles/app.module.css";
 import y from "@/styles/mykhanh.module.css";
 import AppTable from "@/components/app.table";
-import { useEffect } from "react";
+import useSWR from "swr";
+// import { useEffect } from "react";
 
 export default function Home() {
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("http://localhost:8000/blogs");
-      const data = await res.json();
-      console.log(data);
+  const fetcher = (url:string) => fetch(url)
+  .then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:8000/blogs",
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false
     }
-    fetchData()
-  }, []);
+  );
+
+  console.log(">>> check data", data);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch("http://localhost:8000/blogs");
+  //     const data = await res.json();
+  //     console.log(data);
+  //   }
+  //   fetchData()
+  // }, []);
 
   return (
     <div>
+      <div>{data?.length}</div>
       <ul>
         <li className={x.red}>
           <Link href={"/facebook"}>
