1e6edcff4fea632e4974c5b950134d83c554a917
nguyenkhanh0310 <mykhanh03102003@gmail.com>
2024-11-29 11:45:55 +0700

    Final view detail

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,26 +1,43 @@
 "use client";
 import Link from "next/link";
-import Card from 'react-bootstrap/Card';
+import { useParams } from "next/navigation";
+import Card from "react-bootstrap/Card";
+import useSWR, { Fetcher } from "swr";
 
-const ViewDetailBlog = ({ params }: { params: { id: string } }) => {
-  // console.log(">> check searchParams: ", searchParams);
-  // const checkParams = await params;
-  // console.log(">> check params", checkParams);
+const ViewDetailBlog = () => {
+  const params = useParams();
+
+  const fetcher: Fetcher<IBlog, string> = (url: string) =>
+    fetch(url).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(
+    `http://localhost:8000/blogs/${params.id}`,
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    }
+  );
+
+  console.log(">> check data: ", data);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
       <div className="my-3">
-        <Link href={"/blogs"}  className="text-primary">Go back</Link>
+        <Link href={"/blogs"} className="text-primary">
+          Go back
+        </Link>
       </div>
       <Card className="text-center">
-        <Card.Header>Title</Card.Header>
+        <Card.Header>Title: {data?.title}</Card.Header>
         <Card.Body>
-          <Card.Text>
-            With supporting text below as a natural lead-in to additional
-            content.
-          </Card.Text>
+          <Card.Text>{data?.content}</Card.Text>
         </Card.Body>
-        <Card.Footer className="text-muted">author</Card.Footer>
+        <Card.Footer className="text-muted">Author: {data?.author}</Card.Footer>
       </Card>
     </div>
   );
